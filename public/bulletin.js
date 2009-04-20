dc785430165b05c7a516c7d2d44c6734fa53e9f5
towski <towski@gmail.com>
2009-04-20 00:00:44 -0700

    fix

--- public/bulletin.js
+++ public/bulletin.js
@@ -22,7 +22,7 @@ var Bulletin = Class.create({
   buildStates: function(project){
     project = project.project
     var main_div = document.getElementById('bulletin')
-	  var title = new Element('h4', {'style':'margin:auto');
+	  var title = new Element('h4', {'style':'margin:auto'});
 		title.update(project.name + " Current Milestone")
 	  main_div.appendChild(title);
 		var state_divs = 0;
