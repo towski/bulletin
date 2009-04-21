5173e4bd85329237471e63156ce890f2be967a68
towski <towski@gmail.com>
2009-04-20 19:49:33 -0700

    style

--- public/bulletin.js
+++ public/bulletin.js
@@ -140,7 +140,7 @@ var Bulletin = Class.create({
 			membership = membership.membership
 			if(Bulletin.assigned_user_hash[membership.user.name] && !alreadyListed[membership.user.name]){
 				var avatar = new Element('span', {'style':'width:80px;float:left'})
-				avatar.update("<img src='http://entp.lighthouseapp.com/"+membership.user.avatar_url+"' title='"+membership.user.name+"' style='float:left;border:2px solid #"+Bulletin.assigned_user_hash[membership.user.name]+"'/>")
+				avatar.update("<img src='http://entp.lighthouseapp.com/"+membership.user.avatar_url+"' title='"+membership.user.name+"' style='float:left;border:4px solid #"+Bulletin.assigned_user_hash[membership.user.name]+"'/>")
 				avatarBar.appendChild(avatar)
 				alreadyListed[membership.user.name] = true
 			}
