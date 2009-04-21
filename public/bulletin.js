f4c1ae5c1fb72ebdf5deb3eccd736d3e2d25921f
towski <towski@gmail.com>
2009-04-20 19:36:54 -0700

    style

--- public/bulletin.js
+++ public/bulletin.js
@@ -31,13 +31,13 @@ var Bulletin = Class.create({
 		var colors = {open:"aaa", blocked:"a00", verify:"099", resolved:"6A0", staged:"ada"}
 		var open_states = project.open_states_list.split(",")
 		if(!open_states.include("new")){
-		  var state_div = new Element('div', {'id':"new", 'class':'state'});
+		  var state_div = new Element('div', {'id':"new", 'class':'state','style':'clear:both'});
 		  state_div.update("<h6>new</h6>")
 		  main_div.appendChild(state_div);
 			state_divs++;
 		}
     open_states.each(function(state){
-      var state_div = new Element('div', {'id':state, 'class':'state'});
+      var state_div = new Element('div', {'id':state, 'class':'state','style':'clear:both'});
       state_div.update("<h6>"+state+"</h6>")
       main_div.appendChild(state_div);
 			state_divs++;
