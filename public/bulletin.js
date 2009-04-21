dc8433ff8fe5a6ff49cd07ae07aeb532146c6617
towski <towski@gmail.com>
2009-04-20 19:45:13 -0700

    back to span

--- public/bulletin.js
+++ public/bulletin.js
@@ -139,8 +139,8 @@ var Bulletin = Class.create({
 		memberships.each(function(membership){
 			membership = membership.membership
 			if(Bulletin.assigned_user_hash[membership.user.name] && !alreadyListed[membership.user.name]){
-				var avatar = new Element('span', {'class':'float:left', 'style':'width:80px'})
-				avatar.update("<img src='http://entp.lighthouseapp.com/"+membership.user.avatar_url+"' title='"+membership.user.name+"' style='float:left'/>" + ":" + "<div style='width:10px;height:10px;float:left;background:#"+Bulletin.assigned_user_hash[membership.user.name]+"'></div>")
+				var avatar = new Element('span', {'style':'width:80px;float:left'})
+				avatar.update("<img src='http://entp.lighthouseapp.com/"+membership.user.avatar_url+"' title='"+membership.user.name+"' style='float:left'/>" + ":" + "<div style='width:10px;height:10px;float:right;background:#"+Bulletin.assigned_user_hash[membership.user.name]+"'></div>")
 				avatarBar.appendChild(avatar)
 				alreadyListed[membership.user.name] = true
 			}
